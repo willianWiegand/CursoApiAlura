f3e79a17f4eb12e13565bef4703e360f5e20270b
willian.wiegand <willian.wiegand@sequor.com.br>
2024-11-20 16:35:53 -0300

    FEAT: Lesson1

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+app.listen(3000,()=>{
+    console.log("Servidor Escutando...");
+});
+app.get("/api", (req,res)=>{
+    res.status(200).send("OK");
+});
