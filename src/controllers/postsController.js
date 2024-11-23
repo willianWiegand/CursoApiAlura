cb624c76d78c9a0f9b94478231ad24acaf512620
willian.wiegand <willian.wiegand@sequor.com.br>
2024-11-23 17:20:25 -0300

    FEAT: Include a rout to upload images to DB. Lesson 4

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,17 +1,46 @@
 import { getEquipePokemon, IncluirPokemonNovo } from "../models/postsModel.js";
+import fs from "fs";
 
+// Função para listar toda a equipe de Pokémon armazenada no banco de dados.
 export async function listarEquipePokemon (req, res){
-    const equipePokemon =  await getEquipePokemon()
+    // Chama a função do modelo para buscar a equipe de Pokémon.
+    const equipePokemon = await getEquipePokemon();
+    // Retorna a equipe de Pokémon como resposta HTTP com status 200 (sucesso).
     res.status(200).json(equipePokemon);
 }
 
+// Função para incluir um novo Pokémon na equipe.
 export async function IncluirPokemonEquipe(req,res) {
+    // Obtém os dados do novo Pokémon do corpo da requisição.
     const novoPokemon = req.body;
-    try{
+    try {
+        // Chama a função do modelo para inserir o novo Pokémon no banco de dados.
         const pokemonIncluido = await IncluirPokemonNovo(novoPokemon);
-        res.status(200).json(pokemonIncluido);  
-    }catch(erro){
+        // Retorna o Pokémon recém-inserido como resposta HTTP com status 200 (sucesso).
+        res.status(200).json(pokemonIncluido);
+    } catch (erro) {
+        // Caso ocorra algum erro durante a inserção, loga o erro no console e retorna uma mensagem de erro ao usuário.
         console.error(erro.message);
         res.status(500).json({"Erro":"Falha ao enviar"});
-    }    
+    }
+}
+
+// Função para fazer upload da imagem de um Pokémon.
+export async function uploadImagem(req,res) {
+    // Obtém os dados do novo Pokémon do corpo da requisição.
+    const novoPokemon = req.body;
+    try {
+        // Insere o novo Pokémon no banco de dados.
+        const pokemonIncluido = await IncluirPokemonNovo(novoPokemon);
+        // Cria o nome do arquivo da imagem com base no ID inserido.
+        const imagemAtualizada = `uploads/${pokemonIncluido.insertedId}.png`;
+        // Move o arquivo da imagem temporária para o destino final.
+        fs.renameSync(req.file.path, imagemAtualizada);
+        // Retorna o Pokémon recém-inserido como resposta HTTP com status 200 (sucesso).
+        res.status(200).json(pokemonIncluido);
+    } catch (erro) {
+        // Caso ocorra algum erro durante o upload, loga o erro no console e retorna uma mensagem de erro ao usuário.
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha ao enviar"});
+    }
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,12 +1,47 @@
+// Importa o framework Express para criar a aplicação web.
 import express from "express";
-import { IncluirPokemonEquipe, listarEquipePokemon } from "../controllers/postsController.js";
 
+// Importa o middleware multer para tratar uploads de arquivos.
+import multer from "multer";
+
+// Importa as funções controladoras do arquivo postsController.js
+import { IncluirPokemonEquipe, listarEquipePokemon, uploadImagem } from "../controllers/postsController.js";
+
+// Configura o armazenamento de arquivos para o multer.
+// - Define a pasta 'uploads/' como destino para os arquivos.
+// - Utiliza o nome original do arquivo como nome final.
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+
+// Cria uma instância do multer utilizando o storage configurado.
+// - Define a pasta temporária para armazenamento inicial dos arquivos.
+const upload = multer({ dest: "./uploads", storage })
+
+// Define as rotas da aplicação.
 const routes = (app) => {
-    app.use(express.json());
-    
-    app.get("/equipePokemon", listarEquipePokemon);
+  // Habilita o middleware express.json() para interpretar requisições JSON.
+  app.use(express.json());
 
-    app.post("/equipePokemon", IncluirPokemonEquipe)
+  // Rota GET para listar a equipe Pokemon.
+  // - Chama a função listarEquipePokemon do controlador.
+  app.get("/equipePokemon", listarEquipePokemon);
+
+  // Rota POST para incluir um novo Pokémon na equipe.
+  // - Chama a função IncluirPokemonEquipe do controlador.
+  // - Recebe dados do novo Pokémon no corpo da requisição.
+  app.post("/equipePokemon", IncluirPokemonEquipe);
+
+  // Rota POST para fazer upload da imagem de um Pokémon.
+  // - Utiliza o middleware upload.single("imagem") para tratar o upload da imagem.
+  // - Chama a função uploadImagem do controlador após o upload da imagem.
+  app.post("/upload", upload.single("imagem"), uploadImagem)
 }
 
+// Exporta a função routes para uso em outros arquivos.
 export default routes;
