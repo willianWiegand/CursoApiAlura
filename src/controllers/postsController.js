a7504e3698336672de96a0789dfc56588c0e250e
willian.wiegand <willian.wiegand@sequor.com.br>
2024-11-23 10:52:59 -0300

    FIX: adjust dependencies and object

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -3,5 +3,5 @@ import { getEquipePokemon } from "../models/postsModels.js";
 export async function listarEquipePokemon (req, res)
 {
     const equipePokemon =  await getEquipePokemon();
-    res.status(200).json(posts);
+    res.status(200).json(equipePokemon);
 }
